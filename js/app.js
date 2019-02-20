79bc1d5151eddb179cc45df3efa6492f68706edf
welkin-qing <welkin.qing@foxmail.com>
2019-02-20 18:00:40 +0800

    完成任务项的编辑及处理

--- js/app.js
+++ js/app.js
@@ -1,6 +1,78 @@
-(function (window) {
-	'use strict';
+;(function(){
+	const todos = [
+		{
+			id: 1,
+			title: '敲代码',
+			completed: true
+		},
+		{
+			id: 2,
+			title: '吃饭',
+			completed: false
+		},
+		{
+			id: 3,
+			title: '睡觉觉',
+			completed: true
+		}
+	]
+	new Vue({
+		data: {
+			todos,
+			currentEditing: null
+		},
+		methods: {
+			handleNewTodoKeyDown(e) {
+				//e  事件处理函数
+				const target = e.target
+				const value = target.value.trim() // 删除字符串开始和末尾的空格 
+				if(!value.length){
+					return
+				}
+				const todos = this.todos
+				todos.push({
+					id: todos.length ? todos[todos.length - 1].id + 1 : 1,//元素是空的就为1，否则就为最后一个元素的id+1，
+					title: value,
+					completed: false
+				})
+				target.value = ''
+			},
 
-	// Your starting point. Enjoy the ride!
+			handleToggleAllChange(e) {
+				const checked = e.target.checked
+				this.todos.forEach(item => {
+					item.completed = checked
+				})
+			},
+			//当事件处理函数没有传参的时候，第一个参数就是默认的事件源对象event，当手动传递了参数时，就没有办法获取默认的event事件源对象
+			//所以需要手动调用$event来接收event事件源对象
+			handleRemoveTodoClick(index, e) {
+				this.todos.splice(index, 1)
+			},
 
-})(window);
+			handleGetEditingDblclick(todo) {
+				// 把这个变量等于当前双击的 todo
+				this.currentEditing = todo
+			},
+			//编辑任务敲回车则保存
+			handleSaveEditKeydown(todo, index, e){
+				const target = e.target
+				const value = target.value.trim()
+				if(!value.length){
+					this.todos.splice(index, 1)
+				}else{
+					todo.title = value
+					this.currentEditing = null
+				}
+			},
+
+			handleCancelEsc(){
+				this.currentEditing = null
+			},
+			handleClearAllDoneClick(){
+				//把需要的结果重新过滤出来赋值给todos中
+				this.todos = this.todos.filter(t => !t.completed)
+			}
+		},
+	}).$mount('#app')
+})()
--- readme.md
+++ readme.md
@@ -5,3 +5,19 @@ https://github.com/tastejs/todomvc-app-template
 cnpm install --save-dev browser-sync
 #### 注意
 cnpm install --production 可以只装dependencies中的包
+
+#  添加任务
+1. 敲回车添加到任务列表中
+2. 判断是否为非空数据，若是，则不添加；添加完成后清空文本框
+
+# 任务项
+1. 联动样式，切换任务完成状态
+2. 删除任务项
+3. 双击label编辑任务项
+
+# 编辑任务项
+1. 编辑文本自动获得焦点
+2. 敲回车或者失去焦点则保存文本
+3. 输入状态下，esc取消编辑
+4. 点击清楚完成项，清除所有已完成任务
+
